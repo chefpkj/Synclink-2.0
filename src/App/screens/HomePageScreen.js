339299aebbd1cc87a7a871b8e6ee426de70c776c
Piyush K Jha <chefpkj@Piyushs-Mac-mini.local>
2024-02-25 12:29:10 +0530

    adding files

--- src/App/screens/HomePageScreen.js
+++ src/App/screens/HomePageScreen.js
@@ -4,12 +4,9 @@ import { Link } from 'react-router-dom'
 const HomePageScreen = () => {
   return (
     <>
-    <div>
+    <div className='text-3xl font-bold underline'>
       Home Page
     </div>
-    <Link to='login'>Login</Link>
-    <Link to='add-link'>Add Link</Link>
-    <Link to='viewLink'>View Link</Link>
     </>
   )
 }
